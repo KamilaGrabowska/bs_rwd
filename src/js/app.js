88b2b8e4d650dd848321f70e72114e4479085aaa
KAMILA <dudekkamila@icloud.com>
2023-10-20 12:10:39 +0200

    add html sceletor and css style for calculator section

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+const formRefs = document.querySelectorAll('.calc__form');
+
+// console.log(formRefs)
+
+formRefs.forEach((ref) =>{
+    ref.addEventListener("click", (event) =>{
+        event.target.classList = '#prodducst'
+
+
+    })
+} )
+
+
